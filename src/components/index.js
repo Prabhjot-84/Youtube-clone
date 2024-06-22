5b0d5178b86f10261392c7d77ed88c1192b02c80
Prabhjot-84 <prabhjotsingh1645@gmail.com>
2024-06-22 19:58:06 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+
+// App.js
+
+export {default as Navbar } from './Navbar';
+export {default as Feed } from './Feed';
+export {default as ChannelDetail } from './ChannelDetail';
+export {default as VideoDetail } from './VideoDetail';
+export {default as SearchFeed } from './SearchFeed';
+
+
+// Navbar
+
+export {default as SearchBar} from './SearchBar';
+
+// Feed
+
+export {default as Sidebar} from './Sidebar';
+export {default as Videos} from './Videos';
+
+// Videos
+
+export {default as VideoCard} from './VideoCard';
+export {default as ChannelCard} from './ChannelCard'; 
